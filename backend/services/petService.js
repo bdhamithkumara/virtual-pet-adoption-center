61dd672c2fc5d874697df874eaf62ad2939f0b57
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-05-03 11:06:32 +0530

    feat : added createPet to petServices

---
+++ backend/services/petService.js
@@ -0,0 +1,22 @@
+const {v4 : uuidv4} = require('uuid')
+const petModel = require('../models/petModel')
+
+//mood,adopted,adoption_date
+const createPet = async ({name,species,age,personality}) => {
+    const pet = {
+        id : uuidv4(),
+        name,
+        species,
+        age,
+        personality,
+        mood : 'Happy',
+        adopted : false,
+        adoption_date : null,
+        created_at : new Date().toISOString()
+    }
+    await petModel.addPet(pet)
+}
+
+module.exports = {
+    createPet
+} ;
